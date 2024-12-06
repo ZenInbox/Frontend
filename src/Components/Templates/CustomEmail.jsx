1bb0e829c772db9110b56edd2000a1d487538bab
Priyanshu <priyanshudutta13@gmail.com>
2024-12-06 09:50:03 +0530

    minor fix

--- src/Components/Templates/CustomEmail.jsx
+++ src/Components/Templates/CustomEmail.jsx
@@ -1,4 +1,4 @@
-import React, { useState   } from 'react';
+import React, { useState ,useEffect  } from 'react';
 import { useDropzone } from 'react-dropzone';
 import { FaTrashAlt } from 'react-icons/fa';
 import axios from "axios";
